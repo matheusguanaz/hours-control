9f1106d6e6d38351c95782f064495df1c95029fe
Matheus Guanaz <matheusguanaz@hotmail.com>
2021-12-25 11:01:15 -0300

    creating project

---
+++ src/main/java/com/hourscontrol/hourscontrol/HoursControlApplication.java
@@ -0,0 +1,13 @@
+package com.hourscontrol.hourscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoursControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoursControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hourscontrol/hourscontrol/HoursControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hourscontrol.hourscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoursControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
